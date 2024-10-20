fc5125abbfd8ce401cff860679dbeb1c7d5b939c
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2024-10-20 18:33:24 +0900

    User.nickname の文字数に関するバリデーションを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 # テスト内で一時的に User を作成するための FactoryBot 用ファイル
 FactoryBot.define do
   factory :user do
-    
+    nickname { 'テスト太郎' }
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    password { '111111' }
+    password_confirmation { '111111' }
   end
 end
